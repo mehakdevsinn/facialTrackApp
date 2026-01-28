c0dc2d395c7d56caf3a0ed8e467d4a54b5cbc5ce
mariyaarshad <mariyadevsinn@gmail.com>
2026-01-28 17:59:08 +0500

    fixes

--- lib/view/teacher/Teacher Login/login.dart
+++ lib/view/teacher/Teacher Login/login.dart
@@ -4,6 +4,7 @@ import 'package:facialtrackapp/view/Teacher/Forgot%20Password/forgot-password-sc
 import 'package:facialtrackapp/view/teacher/Teacher_NavBar/teacher_root_screen.dart';
 import 'package:facialtrackapp/widgets/textfield_login.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:lottie/lottie.dart';
 
 class TeacherLoginScreen extends StatefulWidget {
@@ -37,213 +38,219 @@ class _TeacherLoginScreenState extends State<TeacherLoginScreen> {
   bool get isButtonEnabled => teacherId.isNotEmpty && password.isNotEmpty;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-             Container(
-                width: double.infinity,
-                height: 285,
-                decoration: const BoxDecoration(
-                  gradient: LinearGradient(
-                    colors: [
-                      ColorPallet.primaryBlue,
-                      Color.fromARGB(255, 123, 149, 233),
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+    value: const SystemUiOverlayStyle(
+      statusBarColor: Colors.white,           // Dark Background
+      statusBarIconBrightness: Brightness.dark, // Light Icons
+    ),
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: SafeArea(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+               Container(
+                  width: double.infinity,
+                  height: 285,
+                  decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [
+                        ColorPallet.primaryBlue,
+                        Color.fromARGB(255, 123, 149, 233),
+                      ],
+                      begin: Alignment.centerLeft,
+                      end: Alignment.centerRight,
+                    ),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => RoleSelectionScreen(),
+                            ),
+                          );
+                        },
+      
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 20, top: 15),
+                          child: Row(
+                            children: [
+                              Icon(Icons.arrow_back, color: ColorPallet.white),
+                            ],
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 150,
+                        width: 150,
+                        child: Lottie.asset(
+                          'assets/animations/face-detect.json',
+                          repeat: true,
+                          animate: true,
+                        ),
+                      ),
+      
+                      SizedBox(height: 15),
+                      Text(
+                        "Facial Track",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 28,
+                          fontWeight: FontWeight.bold,
+                          letterSpacing: 0.5,
+                        ),
+                      ),
+                      SizedBox(height: 6),
+                      Text(
+                        "Teacher Portal",
+                        style: TextStyle(color: Colors.white70, fontSize: 16),
+                      ),
+      
+                      SizedBox(height: 11),
                     ],
-                    begin: Alignment.centerLeft,
-                    end: Alignment.centerRight,
                   ),
                 ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    GestureDetector(
-                      onTap: () {
+       const SizedBox(height: 40),
+      
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25),
+                  child: buildTextField(
+                    activeColor: ColorPallet.primaryBlue,
+                    inactiveColor: Colors.grey,
+                    focusNode: teacherIdFocus,
+                    onChange: (value) {
+                      setState(() {
+                        teacherId = value;
+                      });
+                    },
+                    label: "Teacher ID",
+                    hint: "Enter your Teacher ID",
+                    icon: Icons.person_outline,
+                  ),
+                ),
+      
+                const SizedBox(height: 20),
+      
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25),
+                  child: buildTextField(
+                    activeColor: ColorPallet.primaryBlue,
+                    inactiveColor: Colors.grey,
+                    focusNode: passwordFocus,
+                    onChange: (value) {
+                      setState(() {
+                        password = value;
+                      });
+                    },
+                    label: "Password",
+                    hint: "Enter your password",
+                    icon: Icons.lock_outline,
+                    obscureText: _obscureText,
+                    suffixIcon: IconButton(
+                      icon: Icon(
+                        _obscureText
+                            ? Icons.visibility_off_outlined
+                            : Icons.visibility_outlined,
+                        color: ColorPallet.grey,
+                      ),
+                      onPressed: () {
+                        setState(() => _obscureText = !_obscureText);
+                      },
+                    ),
+                  ),
+                ),
+      
+                Padding(
+                  padding: const EdgeInsets.only(right: 15),
+                  child: Align(
+                    alignment: Alignment.centerRight,
+                    child: TextButton(
+                      onPressed: () {
                         Navigator.push(
                           context,
                           MaterialPageRoute(
-                            builder: (context) => RoleSelectionScreen(),
+                            builder: (context) => ForgotPasswordScreen(),
                           ),
                         );
                       },
-
-                      child: Padding(
-                        padding: const EdgeInsets.only(left: 20, top: 15),
-                        child: Row(
-                          children: [
-                            Icon(Icons.arrow_back, color: ColorPallet.white),
-                          ],
+                      child: const Text(
+                        "Forgot Password?",
+                        style: TextStyle(
+                          color: ColorPallet.primaryBlue,
+                          fontWeight: FontWeight.w700,
                         ),
                       ),
                     ),
-                    SizedBox(
-                      height: 150,
-                      width: 150,
-                      child: Lottie.asset(
-                        'assets/animations/face-detect.json',
-                        repeat: true,
-                        animate: true,
+                  ),
+                ),
+      
+                const SizedBox(height: 39),
+      
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25),
+                  child: SizedBox(
+                    width: double.infinity,
+                    height: 55,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: isButtonEnabled
+                            ? ColorPallet.deepBlue
+                            : Colors.grey.shade400,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        elevation: 3,
+                      ),
+                      onPressed: isButtonEnabled
+                          ? () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => TeacherRootScreen(),
+                                ),
+                              );
+                            }
+                          : SizedBox.shrink,
+                      child: const Text(
+                        "Login",
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
                       ),
                     ),
-
-                    SizedBox(height: 15),
-                    Text(
-                      "Facial Track",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 28,
-                        fontWeight: FontWeight.bold,
-                        letterSpacing: 0.5,
+                  ),
+                ),
+      
+                const SizedBox(height: 20),
+      
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      height: 18,
+                      width: 18,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(50)),
+                        color: ColorPallet.softGreen,
                       ),
+      
+                      child: Icon(Icons.done, color: ColorPallet.white, size: 10),
                     ),
-                    SizedBox(height: 6),
+                    SizedBox(width: 6),
                     Text(
-                      "Teacher Portal",
-                      style: TextStyle(color: Colors.white70, fontSize: 16),
+                      "Secure Login",
+                      style: TextStyle(color: ColorPallet.grey),
                     ),
-
-                    SizedBox(height: 11),
                   ],
                 ),
-              ),
- const SizedBox(height: 40),
-
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25),
-                child: buildTextField(
-                  activeColor: ColorPallet.primaryBlue,
-                  inactiveColor: Colors.grey,
-                  focusNode: teacherIdFocus,
-                  onChange: (value) {
-                    setState(() {
-                      teacherId = value;
-                    });
-                  },
-                  label: "Teacher ID",
-                  hint: "Enter your Teacher ID",
-                  icon: Icons.person_outline,
-                ),
-              ),
-
-              const SizedBox(height: 20),
-
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25),
-                child: buildTextField(
-                  activeColor: ColorPallet.primaryBlue,
-                  inactiveColor: Colors.grey,
-                  focusNode: passwordFocus,
-                  onChange: (value) {
-                    setState(() {
-                      password = value;
-                    });
-                  },
-                  label: "Password",
-                  hint: "Enter your password",
-                  icon: Icons.lock_outline,
-                  obscureText: _obscureText,
-                  suffixIcon: IconButton(
-                    icon: Icon(
-                      _obscureText
-                          ? Icons.visibility_off_outlined
-                          : Icons.visibility_outlined,
-                      color: ColorPallet.grey,
-                    ),
-                    onPressed: () {
-                      setState(() => _obscureText = !_obscureText);
-                    },
-                  ),
-                ),
-              ),
-
-              Padding(
-                padding: const EdgeInsets.only(right: 15),
-                child: Align(
-                  alignment: Alignment.centerRight,
-                  child: TextButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => ForgotPasswordScreen(),
-                        ),
-                      );
-                    },
-                    child: const Text(
-                      "Forgot Password?",
-                      style: TextStyle(
-                        color: ColorPallet.primaryBlue,
-                        fontWeight: FontWeight.w700,
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-
-              const SizedBox(height: 39),
-
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25),
-                child: SizedBox(
-                  width: double.infinity,
-                  height: 55,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: isButtonEnabled
-                          ? ColorPallet.deepBlue
-                          : Colors.grey.shade400,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                      elevation: 3,
-                    ),
-                    onPressed: isButtonEnabled
-                        ? () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => TeacherRootScreen(),
-                              ),
-                            );
-                          }
-                        : SizedBox.shrink,
-                    child: const Text(
-                      "Login",
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-
-              const SizedBox(height: 20),
-
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    height: 18,
-                    width: 18,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(50)),
-                      color: ColorPallet.softGreen,
-                    ),
-
-                    child: Icon(Icons.done, color: ColorPallet.white, size: 10),
-                  ),
-                  SizedBox(width: 6),
-                  Text(
-                    "Secure Login",
-                    style: TextStyle(color: ColorPallet.grey),
-                  ),
-                ],
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
